88a9b043d7b671d9b9d32e924d7d06a5cf064d11
carlo.sokol <info.carlosk@gmail.com>
2024-03-05 19:54:52 +0100

    index_blade_Carlo

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,4 +2,90 @@
 
     <x-nav-bar/>
 
+    <div class="container-fluid p-0 shadow">
+        <img src="./img/cover.jpg" class="img-fluid" alt="Cover Photo">
+    </div>
+
+    <div class="container mt-4">
+        <h1 class="mb-4">Le nostre forniture</h1>
+        <h4 class="mb-4">Trova prodotti di qualità a prezzi convenienti solo su Presto.it</h4>
+
+        <div class="row">
+            <!-- Card 1 -->
+            <div class="col-md-4 mb-4">
+                <div class="card">
+                    <img src="https://picsum.photos/20/30" class="card-img-top" alt="Product 1">
+                    <div class="card-body">
+                        <h5 class="card-title">Prodotto 1</h5>
+                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        <p class="card-text">Prezzo: $10</p>
+                    </div>
+                    <div class="card-footer text-muted">Data di creazione: 2024-03-04</div>
+                </div>
+            </div>
+
+            <!-- Card 2 -->
+            <div class="col-md-4 mb-4">
+                <div class="card">
+                    <img src="https://picsum.photos/20/30" class="card-img-top" alt="Product 2">
+                    <div class="card-body">
+                        <h5 class="card-title">Prodotto 2</h5>
+                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        <p class="card-text">Prezzo: $20</p>
+                    </div>
+                    <div class="card-footer text-muted">Data di creazione: 2024-03-04</div>
+                </div>
+            </div>
+
+            <div class="col-md-4 mb-4">
+                <div class="card">
+                    <img src="https://picsum.photos/20/30" class="card-img-top" alt="Product 3">
+                    <div class="card-body">
+                        <h5 class="card-title">Prodotto 3</h5>
+                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        <p class="card-text">Prezzo: $20</p>
+                    </div>
+                    <div class="card-footer text-muted">Data di creazione: 2024-03-04</div>
+                </div>
+            </div>
+
+            <div class="col-md-4 mb-4">
+                <div class="card">
+                    <img src="https://picsum.photos/20/30" class="card-img-top" alt="Product 4">
+                    <div class="card-body">
+                        <h5 class="card-title">Prodotto 4</h5>
+                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        <p class="card-text">Prezzo: $20</p>
+                    </div>
+                    <div class="card-footer text-muted">Data di creazione: 2024-03-04</div>
+                </div>
+            </div>
+            
+            <div class="col-md-4 mb-4">
+                <div class="card">
+                    <img src="https://picsum.photos/20/30" class="card-img-top" alt="Product 5">
+                    <div class="card-body">
+                        <h5 class="card-title">Prodotto 5</h5>
+                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        <p class="card-text">Prezzo: $20</p>
+                    </div>
+                    <div class="card-footer text-muted">Data di creazione: 2024-03-04</div>
+                </div>
+            </div>
+
+            <div class="col-md-4 mb-4">
+                <div class="card">
+                    <img src="https://picsum.photos/20/30" class="card-img-top" alt="Product 6">
+                    <div class="card-body">
+                        <h5 class="card-title">Prodotto 6</h5>
+                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        <p class="card-text">Prezzo: $20</p>
+                    </div>
+                    <div class="card-footer text-muted">Data di creazione: 2024-03-04</div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
 </x-layout>
