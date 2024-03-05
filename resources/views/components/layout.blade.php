ba5872450e4fa74a3f2a9d11a02d33285061e3d0
carlo.sokol <info.carlosk@gmail.com>
2024-03-05 18:51:11 +0100

    NavBar_Carlo

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,10 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
-    <h1>
-        Ciao, questa e' la paggina di walcome
-    </h1>
+    {{$slot}}
 </body>
 </html>
--- resources/views/components/nav-bar.blade.php
+++ resources/views/components/nav-bar.blade.php
@@ -1,3 +1,21 @@
-<div>
-    <!-- Life is available only in the present moment. - Thich Nhat Hanh -->
-</div>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+
+    <div class="container">
+      <a class="navbar-brand" href="#">Presto.it</a>
+  
+
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="#">1. Annunci</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">2. Registrati</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">3. Login</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
