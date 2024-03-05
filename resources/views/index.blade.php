58c19f7288b2b421f5b3585ebbb5a250f919c579
carlo.sokol <info.carlosk@gmail.com>
2024-03-05 20:33:30 +0100

    ForEach_Annunci_Carlo

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -11,80 +11,19 @@
         <h4 class="mb-4">Trova prodotti di qualità a prezzi convenienti solo su Presto.it</h4>
 
         <div class="row">
-            <!-- Card 1 -->
-            <div class="col-md-4 mb-4">
-                <div class="card">
-                    <img src="https://picsum.photos/20/30" class="card-img-top" alt="Product 1">
-                    <div class="card-body">
-                        <h5 class="card-title">Prodotto 1</h5>
-                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                        <p class="card-text">Prezzo: $10</p>
+            @foreach($ultimi_annunci as $annuncio)
+                <div class="col-md-4 mb-4">
+                    <div class="card">
+                        <img src="{{ $annuncio['immagine'] }}" class="card-img-top" alt="{{ $annuncio['titolo'] }}">
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $annuncio['titolo'] }}</h5>
+                            <p class="card-text">{{ $annuncio['descrizione'] }}</p>
+                            <p class="card-text">Prezzo: {{ $annuncio['prezzo'] }}</p>
+                        </div>
+                        <div class="card-footer text-muted">Data di creazione: {{ $annuncio['data_creazione'] }}</div>
                     </div>
-                    <div class="card-footer text-muted">Data di creazione: 2024-03-04</div>
                 </div>
-            </div>
-
-            <!-- Card 2 -->
-            <div class="col-md-4 mb-4">
-                <div class="card">
-                    <img src="https://picsum.photos/20/30" class="card-img-top" alt="Product 2">
-                    <div class="card-body">
-                        <h5 class="card-title">Prodotto 2</h5>
-                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                        <p class="card-text">Prezzo: $20</p>
-                    </div>
-                    <div class="card-footer text-muted">Data di creazione: 2024-03-04</div>
-                </div>
-            </div>
-
-            <div class="col-md-4 mb-4">
-                <div class="card">
-                    <img src="https://picsum.photos/20/30" class="card-img-top" alt="Product 3">
-                    <div class="card-body">
-                        <h5 class="card-title">Prodotto 3</h5>
-                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                        <p class="card-text">Prezzo: $20</p>
-                    </div>
-                    <div class="card-footer text-muted">Data di creazione: 2024-03-04</div>
-                </div>
-            </div>
-
-            <div class="col-md-4 mb-4">
-                <div class="card">
-                    <img src="https://picsum.photos/20/30" class="card-img-top" alt="Product 4">
-                    <div class="card-body">
-                        <h5 class="card-title">Prodotto 4</h5>
-                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                        <p class="card-text">Prezzo: $20</p>
-                    </div>
-                    <div class="card-footer text-muted">Data di creazione: 2024-03-04</div>
-                </div>
-            </div>
-            
-            <div class="col-md-4 mb-4">
-                <div class="card">
-                    <img src="https://picsum.photos/20/30" class="card-img-top" alt="Product 5">
-                    <div class="card-body">
-                        <h5 class="card-title">Prodotto 5</h5>
-                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                        <p class="card-text">Prezzo: $20</p>
-                    </div>
-                    <div class="card-footer text-muted">Data di creazione: 2024-03-04</div>
-                </div>
-            </div>
-
-            <div class="col-md-4 mb-4">
-                <div class="card">
-                    <img src="https://picsum.photos/20/30" class="card-img-top" alt="Product 6">
-                    <div class="card-body">
-                        <h5 class="card-title">Prodotto 6</h5>
-                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                        <p class="card-text">Prezzo: $20</p>
-                    </div>
-                    <div class="card-footer text-muted">Data di creazione: 2024-03-04</div>
-                </div>
-            </div>
-
+            @endforeach
         </div>
     </div>
 
