8b3ffeecc3d4db8ce1792eec7951a2ba40d1e70b
carlo.sokol <info.carlosk@gmail.com>
2024-03-07 00:45:13 +0100

    Carlo_Fine_User_story_2

--- resources/views/announcements/index.blade.php
+++ resources/views/announcements/index.blade.php
@@ -1,5 +1,4 @@
 <x-layout>
-<<<<<<< HEAD
 
     <x-nav-bar/>
 
@@ -29,14 +28,4 @@
     </div>
 
 
-=======
-    <x-nav-bar/>
-    <div class="container">
-        <div class="row">
-            <div class="col-12">
-                <h1>Adesso sono stanco, domani facciamo vedere gli annunci</h1>
-            </div>
-        </div>
-    </div>
->>>>>>> 58a66433507a87b33e2ea7b1661191e49de8f638
 </x-layout>
