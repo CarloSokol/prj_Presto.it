d388b2cfd80e9e14c44e70e36d447bd39bb52f4b
Luigiresta <luigiresta2@gmail.com>
2024-03-07 19:37:28 +0100

    Login, Register_Luigi

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -9,36 +9,33 @@ Registrati
 <div class="container mt-5">
         <div class="row">
             <div class="col-md-4 mx-auto">
-                <div class="card">
-                    <div class="card-header">
-                        Registrati
-                    </div>
+                <div class="">
+                    
                     <div class="card-body">
                         <form action="/register" method="POST">
+                            <div class="card-header text-muted h3 mb-3 text-decoration-underline">
+                                Registrati
+                            </div>
                             @csrf
                             <div class="row g-3">
                                 <div class="col-12">
-                                    <label for="name">Nome</label>
-                                    <input type="text" name="name" id="name" class="form-control">
+                                    <input type="text" name="name" id="name" class="form-control" placeholder="Name">
                                     @error('name') <span class="text-danger small">{{ $message }}</span> @enderror
                                 </div>
                                 <div class="col-12">
-                                    <label for="email">Email</label>
-                                    <input type="email" name="email" id="email" class="form-control">
+                                    <input type="email" name="email" id="email" class="form-control" placeholder="Email">
                                     @error('email') <span class="text-danger small">{{ $message }}</span> @enderror
                                 </div>
                                 <div class="col-12">
-                                    <label for="password">Password</label>
-                                    <input type="password" name="password" id="password" class="form-control">
+                                    <input type="password" name="password" id="password" class="form-control" placeholder="Password">
                                     @error('password') <span class="text-danger small">{{ $message }}</span> @enderror
                                 </div>
                                 <div class="col-12">
-                                    <label for="password_confirmation">Conferma Password</label>
-                                    <input type="password" name="password_confirmation" id="password_confirmation" class="form-control">
+                                    <input type="password" name="password_confirmation" id="password_confirmation" class="form-control" placeholder="Conferma Password">
                                 </div>
                                 <div class="col-12">
-                                    <button type="submit" class="btn btn-primary">Registrati</button>
-                                    <p class="small mt-2">Già registrato? <a href="{{ route('login') }}">Clicca qui</a></p>
+                                    <button type="submit" class="btn btn-outline-light">Registrati</button>
+                                    <p class="small mt-2 text-black">Già registrato? <a href="{{ route('login') }}" class="small mt-2 text-dark  ">Clicca qui</a></p>
                                 </div>
                             </div>
                         </form>
