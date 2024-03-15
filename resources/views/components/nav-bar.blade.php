28a33303d0b86c45738e0150bfca5e0700b1bcb0
Andrea.Labianca <andr3a1998@hotmail.it>
2024-03-16 00:41:33 +0100

    Modifiche

--- resources/views/components/nav-bar.blade.php
+++ resources/views/components/nav-bar.blade.php
@@ -1,9 +1,9 @@
 <nav class="navbar navbar-expand-lg shadow-lg bg-secondary">
     <div class="container">
 
-        <a class="navbar-brand w-50 " href="/">
-            <img src="/img/prestoit_logo.png" alt="Logo Presto.it" width="30%">
-        </a>
+        <div class="navbar-brand" style="width:20%">
+            <a href="/"> <img src="/img/prestoit_logo.png" alt="Logo Presto.it" width="150px"></a>
+        </div>
 
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar-nav"
             aria-controls="navbar-nav" aria-expanded="false" aria-label="Toggle navigation">
@@ -18,65 +18,66 @@
                 </li>
 
                 @if (Auth::user() && Auth::user()->is_revisor)
-                <li class="nav-item me-1">
-                    <a class="nav-link courgette-regular position-relative" aria-turrent="page"
-                        href="{{ route('revisor.index') }}">
-                        Zona revisore
-                        <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
-                            {{ App\Models\Announcement::toBeRevisionedCount() }}
-                            <span class="visually-hidden">messaggi non letti</span>
-                        </span>
-                    </a>
-                </li>
+                    <li class="nav-item me-1">
+                        <a class="nav-link courgette-regular position-relative" aria-turrent="page"
+                            href="{{ route('revisor.index') }}">
+                            Zona revisore
+                            <span
+                                class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+                                {{ App\Models\Announcement::toBeRevisionedCount() }}
+                                <span class="visually-hidden">messaggi non letti</span>
+                            </span>
+                        </a>
+                    </li>
                 @endif
 
                 @auth
 
-                <li class="nav-item dropdown">
-                    <a class="nav-link courgette-regular" aria-current="page"
-                        href="{{ route('announcements.create') }}">Nuovo
-                        Annuncio</a>
-                </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link courgette-regular" aria-current="page"
+                            href="{{ route('announcements.create') }}">Nuovo
+                            Annuncio</a>
+                    </li>
 
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle courgette-regular" href="#" id="categoriesDropdown"
-                        role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Categorie
-                    </a>
-                    <ul class="dropdown-menu" aria-labelledby="categoriesDropdown">
-                        @foreach ($categories as $category)
-                        <li><a class="dropdown-item courgette-regular"
-                                href="{{ route('categoryShow', compact('category')) }}">{{ $category->name }}</a>
-                        </li>
-                        <li>
-                            <hr class="dropdown-divider">
-                        </li>
-                        @endforeach
-                    </ul>
-                </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle courgette-regular" href="#" id="categoriesDropdown"
+                            role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Categorie
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="categoriesDropdown">
+                            @foreach ($categories as $category)
+                                <li><a class="dropdown-item courgette-regular"
+                                        href="{{ route('categoryShow', compact('category')) }}">{{ $category->name }}</a>
+                                </li>
+                                <li>
+                                    <hr class="dropdown-divider">
+                                </li>
+                            @endforeach
+                        </ul>
+                    </li>
 
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle courgette-regular" href="#" role="button"
-                        data-bs-toggle="dropdown" aria-expanded="false">
-                        Benvenuto {{ Auth::user()->name }}
-                    </a>
-                    <ul class="dropdown-menu">
-                        <li class="nav-item ">
-                            <form action="{{ route('logout') }}" id="logout-form" method="POST">
-                                @csrf
-                                <button type="submit" class="btn text-center w-100 "> <strong>Logout</strong>
-                                </button>
-                            </form>
-                        </li>
-                    </ul>
-                </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle courgette-regular" href="#" role="button"
+                            data-bs-toggle="dropdown" aria-expanded="false">
+                            Benvenuto {{ Auth::user()->name }}
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li class="nav-item ">
+                                <form action="{{ route('logout') }}" id="logout-form" method="POST">
+                                    @csrf
+                                    <button type="submit" class="btn text-center w-100 "> <strong>Logout</strong>
+                                    </button>
+                                </form>
+                            </li>
+                        </ul>
+                    </li>
                 @else
-                <li class="nav-item">
-                    <a class="nav-link  courgette-regular" href="{{ route('register') }}">Registrati</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link  courgette-regular" href="{{ route('login') }}">Login</a>
-                </li>
+                    <li class="nav-item">
+                        <a class="nav-link  courgette-regular" href="{{ route('register') }}">Registrati</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link  courgette-regular" href="{{ route('login') }}">Login</a>
+                    </li>
 
                 @endauth
 
