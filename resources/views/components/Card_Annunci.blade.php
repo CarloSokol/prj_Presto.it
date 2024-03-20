b336b8fedd6c2e595a7f49b105932bc0e6975552
carlo.sokol <info.carlosk@gmail.com>
2024-03-20 21:39:01 +0100

    DEAL_CARLO

--- resources/views/components/Card_Annunci.blade.php
+++ resources/views/components/Card_Annunci.blade.php
@@ -44,3 +44,49 @@
         </div>
     </div>
 @endforeach
+
+<section class=" container bg-danger-subtle bg-cta py-5">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-lg-6">
+                <div class="py-lg-5">
+                    <p class="text-uppercase badge bg-danger-subtle text-danger fs-13">Get <b>50%</b> off to your order</p>
+                    <h1 class="lh-base fw-semibold mb-3 text-capitalize">Deal of the Week</h1>
+                    <p class="fs-16 mt-2">The hands-down winner of denim-friendly sandal styles has to be flat and simple thong sandals. They can be paired with virtually every style of women's jeans imaginable, and, as long as you can stand the toe strap, they tend to be really comfortable as well.</p>
+                    <div class="row align-items-center mt-4">
+                        <div class="col-lg-10">
+                            <div class="ecommerce-land-countdown mt-3 mb-0">
+                                <div data-countdown="Jan 30, 2025" class="countdownlist">
+                                    <div class="countdownlist-item">
+                                        <div class="count-title">Days</div>
+                                        <div class="count-num">315</div>
+                                    </div>
+                                    <div class="countdownlist-item">
+                                        <div class="count-title">Hours</div>
+                                        <div class="count-num">2</div>
+                                    </div>
+                                    <div class="countdownlist-item">
+                                        <div class="count-title">Minutes</div>
+                                        <div class="count-num">32</div>
+                                    </div>
+                                    <div class="countdownlist-item">
+                                        <div class="count-title">Seconds</div>
+                                        <div class="count-num">6</div>
+                                    </div>
+                                </div>
+                                <div class="mt-4 pt-2 d-flex gap-2">
+                                    <a href="#!" class="btn btn-primary w-md btn-hover">Shop Now</a>
+                                    <a href="#!" class="btn btn-danger w-md btn-hover">Subscribe</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6 d-none d-lg-block">
+                <img src="../assets/images/ecommerce/home/cta.png" alt="" class="mt-lg-n5">
+            </div>
+        </div>
+    </div>
+</section>
+
