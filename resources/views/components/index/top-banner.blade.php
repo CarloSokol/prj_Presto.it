a2ddb3908b52e678ac9c5802213b61904a02613e
Andrea.Labianca <andr3a1998@hotmail.it>
2024-03-21 21:33:41 +0100

    topbanner

---
+++ resources/views/components/index/top-banner.blade.php
@@ -0,0 +1,50 @@
+<section class="section bg-topbanner">
+    <div class="container">
+        <div class="row gy-4 gy-lg-0">
+            <div class="col-lg-3 col-sm-6">
+                <div class="d-flex align-items-center">
+                    <div class="flex-shrink-0">
+                        <img src="./img/fast-delivery.png" alt="" class="w-50" >
+                    </div>
+                    <div class="flex-grow-1">
+                        <h5 class="fs-15">Fast &amp; Secure Delivery</h5>
+                        <p class="text-muted mb-0">Tell about your service.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-sm-6">
+                <div class="d-flex align-items-center ">
+                    <div class="flex-shrink-0">
+                        <img src="./img/returns.png" alt="" class="w-50">
+                    </div>
+                    <div class="flex-grow-1">
+                        <h5 class="fs-15">2 Days Return Policy</h5>
+                        <p class="text-muted mb-0">No question ask.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-sm-6">
+                <div class="d-flex align-items-center ">
+                    <div class="flex-shrink-0">
+                        <img src="./img/guarantee-certificate.png" alt="" class="w-50">
+                    </div>
+                    <div class="flex-grow-1">
+                        <h5 class="fs-15">Money Back Guarantee</h5>
+                        <p class="text-muted mb-0">Within 5 business days</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-sm-6">
+                <div class="d-flex align-items-center">
+                    <div class="flex-shrink-0">
+                        <img src="./img/24-hours-support.png" alt="" class="w-50">
+                    </div>
+                    <div class="flex-grow-1">
+                        <h5 class="fs-15">24 X 7 Service</h5>
+                        <p class="text-muted mb-0">Online service for customer</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
