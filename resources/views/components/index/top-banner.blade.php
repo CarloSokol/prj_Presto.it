4cf2dbacf9e4d04fefc85f734e4ec5fcac163efe
Andrea.Labianca <andr3a1998@hotmail.it>
2024-03-21 23:35:36 +0100

    modifiche topbanner

--- resources/views/components/index/top-banner.blade.php
+++ resources/views/components/index/top-banner.blade.php
@@ -1,4 +1,4 @@
-<section class="section bg-topbanner">
+<section class="section bg-topbanner mb-3">
     <div class="container">
         <div class="row gy-4 gy-lg-0">
             <div class="col-lg-3 col-sm-6">
@@ -6,7 +6,7 @@
                     <div class="flex-shrink-0">
                         <img src="./img/fast-delivery.png" alt="" class="w-50" >
                     </div>
-                    <div class="flex-grow-1">
+                    <div class="flex-grow-1 m-topbanner">
                         <h5 class="fs-15">Fast &amp; Secure Delivery</h5>
                         <p class="text-muted mb-0">Tell about your service.</p>
                     </div>
@@ -17,7 +17,7 @@
                     <div class="flex-shrink-0">
                         <img src="./img/returns.png" alt="" class="w-50">
                     </div>
-                    <div class="flex-grow-1">
+                    <div class="flex-grow-1 m-topbanner">
                         <h5 class="fs-15">2 Days Return Policy</h5>
                         <p class="text-muted mb-0">No question ask.</p>
                     </div>
@@ -28,7 +28,7 @@
                     <div class="flex-shrink-0">
                         <img src="./img/guarantee-certificate.png" alt="" class="w-50">
                     </div>
-                    <div class="flex-grow-1">
+                    <div class="flex-grow-1 m-topbanner">
                         <h5 class="fs-15">Money Back Guarantee</h5>
                         <p class="text-muted mb-0">Within 5 business days</p>
                     </div>
@@ -39,7 +39,7 @@
                     <div class="flex-shrink-0">
                         <img src="./img/24-hours-support.png" alt="" class="w-50">
                     </div>
-                    <div class="flex-grow-1">
+                    <div class="flex-grow-1 m-topbanner">
                         <h5 class="fs-15">24 X 7 Service</h5>
                         <p class="text-muted mb-0">Online service for customer</p>
                     </div>
